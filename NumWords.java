cc117450d131c479ff90dd11126b63f79e6bf67f
Tomer Elankry <tomerelankry@Tomers-MacBook-Pro.local>
2024-11-07 12:37:27 +0200

    completed

--- NumWords.java
+++ NumWords.java
@@ -2,6 +2,7 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int nw = Integer.parseInt(args[0]);
-		System.out.println(nw%1000/100 +" hundreds, "+(nw%100)/10+" tens, "+nw%10+" ones");
+		System.out.println(nw/100 +" hundreds, "+(nw%100)/10+" tens, and "+nw%10+" ones.");
+		//1 hundreds, 1 tens, and 0 ones.
 	}
 }
