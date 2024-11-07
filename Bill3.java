184a3a80941124f501e0f1a4890a45e44118447a
Tomer Elankry <tomerelankry@Tomers-MacBook-Pro.local>
2024-11-07 11:41:51 +0200

    completed

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,13 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int price = Integer.parseInt(args[3]);
+		//Basiclly, I've recieved 3 names and the price of the bill.
+		double fixed = Math.ceil(price/3.0); // Here I've wanted to round the price up.
+		System.out.println("Dear " + name1 + ", " + name2 + ", " + name3 + ": pay " + fixed + " Shekels each.");
+		
+		
 	}
 }
